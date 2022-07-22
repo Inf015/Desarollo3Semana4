a9d3a228a5f515555c7ca77975e155c282f7a28d
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2022-07-21 20:26:04 -0400

    All

---
+++ Semana 4 Oliver 1100292/Semana 4 Oliver 1100292/Program.cs
@@ -0,0 +1,24 @@
+﻿using Semana_4_Oliver_1100292.dsPeruTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Semana_4_Oliver_1100292
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AfectadosTableAdapter adapter = new AfectadosTableAdapter();
+            EventoTableAdapter adapter1 = new EventoTableAdapter();
+
+
+            adapter1.ppInsertEvento("Terremoto 7.0", "Terremoto", DateTime.Parse("05/06/2022"), "Peru", "Gran terremoto en peru que dejo muchos heridos");
+            adapter.ppInsertAfectados("Cedula","Alicia","Morel",DateTime.Parse("23/02/2002"),"Mujer",DateTime.Now,"Cabeza",1);
+            Console.WriteLine("listo");
+        }
+    }
+}
---
+++ Semana 4 Oliver 1100292/Semana 4 Oliver 1100292/dsPeru.cs
@@ -0,0 +1,21 @@
+﻿namespace Semana_4_Oliver_1100292
+{
+
+
+    partial class dsPeru
+    {
+        partial class AfectadosDataTable
+        {
+        }
+    }
+}
+
+namespace Semana_4_Oliver_1100292.dsPeruTableAdapters
+{
+    partial class AfectadosTableAdapter
+    {
+    }
+
+    public partial class EventoTableAdapter {
+    }
+}
